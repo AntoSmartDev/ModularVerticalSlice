1949c498b4967f8d67083850a6be641b9018f86e
anton <anton@ANTODEV>
2026-05-23 17:31:03 +0200

    test(messaging): add booking saga durable runtime coverage

--- tests/ModularVerticalSlice.UnitTests/Modules/Bookings/BookingLifecycleApiBaselineTests.cs
+++ tests/ModularVerticalSlice.UnitTests/Modules/Bookings/BookingLifecycleApiBaselineTests.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using ModularVerticalSlice.Application.Modules.Bookings;
